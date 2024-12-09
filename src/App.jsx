9acbb113e7492c1de4dd2d205a06d96a8e73450a
Paras Bagri <parasba85@gmail.com>
2024-12-10 00:49:53 +0530

    build production level

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import './App.css'
 function App() {
   const [text, setText] = useState("");
   const [audio, setAudio] = useState(null);
+  const [voice, setVoice] = useState("female");
 
   const handleSynthesize = async () => {
     try {
        // Clear the existing audio before generating a new one
        setAudio(null);
        
-      const response = await axios.post("http://localhost:5000/synthesize", { text }, {
+      const response = await axios.post("https://voice-backend-3.onrender.com/synthesize", { text }, {
         responseType: "blob", 
       });
       const audioUrl = URL.createObjectURL(response.data);
@@ -32,15 +33,47 @@ function App() {
         onChange={(e) => setText(e.target.value)}
       />
       <br />
+      {/* <div>
+      <label>
+          <input 
+          type="radio"
+          name="voice"
+          value="male"
+          checked={voice === "male"}
+          onChange={(e)=> setVoice(e.target.value)}
+          />
+          Male
+        </label>
+
+
+        <label>
+          <input type="radio"
+          name="voice"
+          value="female"
+          checked={voice === "female"}
+          onChange={(e)=> setVoice(e.target.value)}
+          />
+          Female
+        </label>
+      </div> */}
+
       <button onClick={handleSynthesize}>Convert to Audio</button>
       {audio && (
         <div>
-          <h2>Generated Audio:</h2>
-          <audio controls>
+          <h2>Generated Audio</h2>
+          <audio controls autoPlay>
             <source src={audio} type="audio/mpeg" />
           </audio>
         </div>
       )}
+      {/* <div>
+        <label htmlFor="male" name="voice">Male</label>
+        <input type="radio" />
+      </div>
+      <div>
+        <label htmlFor="female"name="voice">Female</label>
+        <input type="radio" />
+      </div> */}
     </div>
   );
 }
